22fe8d743d69d01b9ad214e4a79e2047ac531d56
fpdevil <singamsetty.sampath@gmail.com>
2021-01-16 23:31:28 -0600

    minor updates for printing results

--- random-stuff/gopl/01-fetchurl/main.go
+++ random-stuff/gopl/01-fetchurl/main.go
@@ -19,7 +19,7 @@ func main() {
 		body, err := ioutil.ReadAll(res.Body)
 		res.Body.Close()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "url fetch: read %v\n", err)
+			fmt.Fprintf(os.Stderr, "url fetch: reading %s: %v\n", url, err)
 			return
 		}
 		fmt.Println(string(body))
